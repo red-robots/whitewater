afb89bbdb1b9d19dc01b987bdca8952cb2d66d58
Austin Crane <austincrane@gmail.com>
2019-02-18 10:54:57 -0500

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,142 @@
+/**
+ *	Custom jQuery Scripts
+ *	
+ *	Developed by: Austin Crane	
+ *	Designed by: Austin Crane
+ */
+
+jQuery(document).ready(function ($) {
+	/*
+	*
+	*	Sticky Header
+	*
+	------------------------------------*/
+	$( window ).scroll(function() {
+		$( ".mobile-header-scroll" ).addClass( "scrolled");
+		$( "#masthead" ).addClass( "scrolled");
+	});
+	/*
+	*
+	*	Current Page Active
+	*
+	------------------------------------*/
+	$("[href]").each(function() {
+    if (this.href == window.location.href) {
+        $(this).addClass("active");
+        }
+	});
+	$('li.menu-item-has-children').click( function() {
+        $(this).find('ul.sub-menu').css('maxHeight:1000');
+    });
+	/*
+        FAQ dropdowns
+	__________________________________________
+	*/
+	$('.question').click(function() {
+	 
+	    $(this).next('.answer').slideToggle(500);
+	    $(this).toggleClass('close');
+	    $(this).find('.plus-minus-toggle').toggleClass('collapsed');
+	    $(this).parent().toggleClass('active');
+	 
+	});
+	/*
+	*
+	*	Mobile Menu
+	*
+	------------------------------------*/
+	$('.burger, .overlay').click(function(){
+		$('.burger').toggleClass('clicked');
+		$('.overlay').toggleClass('show');
+		$('nav').toggleClass('show');
+		$('body').toggleClass('overflow');
+	});
+	$('nav.mobilemenu li').click(function() {
+		$(this).find('ul.dropdown').toggleClass('active');
+	});
+
+	/*
+	*
+	*	Responsive iFrames
+	*
+	------------------------------------*/
+	var $all_oembed_videos = $("iframe[src*='youtube']");
+	
+	$all_oembed_videos.each(function() {
+	
+		$(this).removeAttr('height').removeAttr('width').wrap( "<div class='embed-container'></div>" );
+ 	
+ 	});
+	
+	/*
+	*
+	*	Flexslider
+	*
+	------------------------------------*/
+	$('.flexslider').flexslider({
+		animation: "slide",
+	}); // end register flexslider
+	
+	/*
+	*
+	*	Colorbox
+	*
+	------------------------------------*/
+	$('a.gallery').colorbox({
+		rel:'gal',
+		width: '80%', 
+		height: '80%'
+	});
+	
+	/*
+	*
+	*	Isotope with Images Loaded
+	*
+	------------------------------------*/
+	var $container = $('#container').imagesLoaded( function() {
+  	$container.isotope({
+    // options
+	 itemSelector: '.item',
+		  masonry: {
+			gutter: 15
+			}
+ 		 });
+	});
+
+	/*
+	*
+	*	Smooth Scroll to Anchor
+	*
+	------------------------------------*/
+	 $('a').click(function(){
+	    $('html, body').animate({
+	        scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+	    }, 500);
+	    return false;
+	});
+
+	/*
+	*
+	*	Nice Page Scroll
+	*
+	------------------------------------*/
+	// $(function(){	
+	// 	$("html").niceScroll();
+	// });
+	
+	
+	/*
+	*
+	*	Equal Heights Divs
+	*
+	------------------------------------*/
+	$('.js-blocks').matchHeight();
+
+	/*
+	*
+	*	Wow Animation
+	*
+	------------------------------------*/
+	new WOW().init();
+
+});// END #####################################    END
---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package ACStarter
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="wrapper">
+			<div class="site-info">
+				
+			</div><!-- .site-info -->
+	</div><!-- wrapper -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
